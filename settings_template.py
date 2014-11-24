3c502910f25a30ba8da68e29d646e5396ba1c772
SteveKueng <kuengst@StevesMacBookPro.local>
2014-11-24 21:12:23 +0100

    changed git default setting

--- settings_template.py
+++ settings_template.py
@@ -12,7 +12,7 @@ ICONS_DIR = 'icons/'
 # manifest edits to a git repo
 # if GITPATH is undefined or None MunkiWeb will not attempt to do a git add
 # or commit
-GIT_PATH = '/usr/bin/git'
+GIT_PATH = ''
 
 # name of the key in a manifest file that names the user or dept
 MANIFEST_USERNAME_KEY = 'user'
