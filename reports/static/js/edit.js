72e44b7ab5ba5b5ce05037f337323f447ad0972a
SteveKueng <kuengst@stevesmacbookpro.srg-guest.ch>
2014-12-04 19:02:37 +0100

    design fix

--- reports/static/js/edit.js
+++ reports/static/js/edit.js
@@ -308,12 +308,12 @@ function sideSecific() {
 function hideTable(element, button) {
     $('.' + element).addClass("hidden");
     $('#' + button).attr("onclick","showTable('" + element + "', this.id)");
-    $('#' + button).html("<span class=\"glyphicon glyphicon-plus\" aria-hidden=\"true\"></span>");
+    $('#' + button).html("<span class=\"glyphicon glyphicon-expand\" aria-hidden=\"true\"></span>");
 }
 
 function showTable(element, button) {
     $('.' + element).removeClass("hidden");
     $('#' + button).attr("onclick","hideTable('" + element + "', this.id)");
-    $('#' + button).html("<span class=\"glyphicon glyphicon-minus\" aria-hidden=\"true\"></span>");
+    $('#' + button).html("<span class=\"glyphicon glyphicon-collapse-down\" aria-hidden=\"true\"></span>");
 }
 // ---------------------------------------------------------------------------
