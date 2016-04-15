77bfc53b118de1fd962b082cd402b037bf0946ff
Steve Küng <steve.kueng@gmail.com>
2016-04-15 23:04:08 +0200

    fix

--- reports/static/reports/js/clienttable.js
+++ reports/static/reports/js/clienttable.js
@@ -1,12 +1,19 @@
-$(document).ready(function() {
-$('button').on('click',function(e) {
-				alert("test");
-    if($(this).hasClass('list')) {
-			alert("test");
-        $('#deviceList').removeClass('grid').addClass('list');
-    }
-    else if ($(this).hasClass('grid')) {
-        $('#deviceList').removeClass('list').addClass('grid');
-    }
+$(document).on('click','.grid_list', function (e) {
+	    if($(this).hasClass('list')) {
+	        $('#deviceList').removeClass('grid').addClass('list');
+	    }
+	    else if ($(this).hasClass('grid')) {
+	        $('#deviceList').removeClass('list').addClass('grid');
+	    }
 });
-}
+
+$(document).on('keyup','#listSearchField', function () {
+	filter = $(this).val();
+	$("#deviceList li").each(function() {
+    if ($(this).text().search(new RegExp(filter, "i")) > -1) {
+      $(this).show();
+    } else {
+      $(this).hide();
+    }
+  });
+});
