ca0d47161ab81d9f053be3b36eaa2f350b2f86f1
Steve Küng <steve.kueng@srgssr.ch>
2017-11-22 09:52:30 +0100

    fix

--- api/urls.py
+++ api/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^(?P<kind>pkgsinfo$)', api.views.plist_api),
     url(r'^(?P<kind>pkgsinfo)/(?P<filepath>.*$)', api.views.plist_api),
     url(r'^(?P<kind>icons)/(?P<filepath>_icon_hashes.plist$)', api.views.plist_api),
-    url(r'^(?P<kind>icons)', api.views.file_api),
+    url(r'^(?P<kind>icons$)', api.views.file_api),
     url(r'^(?P<kind>icons)/(?P<filepath>.*$)', api.views.file_api),
     url(r'^(?P<kind>pkgs$)', api.views.file_api),
     url(r'^(?P<kind>pkgs)/(?P<filepath>.*$)', api.views.file_api),
