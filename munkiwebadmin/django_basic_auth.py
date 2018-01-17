f00a2679e54d0037f43740f28c468b8f6781996c
Steve Küng <steve.kueng@srgssr.ch>
2018-01-17 08:10:17 +0100

    fix issue “domain\user” in basic auth

--- munkiwebadmin/django_basic_auth.py
+++ munkiwebadmin/django_basic_auth.py
@@ -35,6 +35,7 @@ def view_or_basicauth(view, request, test_func, realm="", *args, **kwargs):
                 try:
                     uname, passwd = base64.b64decode(
                         auth[1]).decode('utf-8').split(':', 1)
+                    uname = uname.split('\\', 1)[-1]
                     user = authenticate(username=uname, password=passwd)
                     if user is not None:
                         if user.is_active:
