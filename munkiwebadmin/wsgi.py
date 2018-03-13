c9b8ba12bfcd0698dc19786ae859394124a92b11
Steve Küng <steve.kueng@srgssr.ch>
2018-03-13 10:52:40 +0100

    add proxy environment variables

--- munkiwebadmin/wsgi.py
+++ munkiwebadmin/wsgi.py
@@ -11,6 +11,14 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+cert = os.path.join('/usr/local/share/ca-certificates','ca-certificates.crt')
+if os.path.exists(cert):
+    os.environ['REQUESTS_CA_BUNDLE'] = cert
+
+os.environ['http_proxy'] = os.getenv('PROXY_ADDRESS')
+os.environ['https_proxy'] = os.getenv('PROXY_ADDRESS')
+os.environ['no_proxy'] = os.getenv('NO_PROXY')
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "munkiwebadmin.settings")
 
 application = get_wsgi_application()
