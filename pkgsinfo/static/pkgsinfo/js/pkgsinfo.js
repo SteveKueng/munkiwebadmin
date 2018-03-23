2262dab12b1564714a2adc48973fda44069a8444
Steve Küng <steve.kueng@srgssr.ch>
2018-03-23 14:56:25 +0100

    add makecatalogs

--- pkgsinfo/static/pkgsinfo/js/pkgsinfo.js
+++ pkgsinfo/static/pkgsinfo/js/pkgsinfo.js
@@ -41,6 +41,7 @@ $(document).ready(function() {
     $('#mass_delete').on('click', confirmMassDelete);
     $('#massaction_dropdown').on('click', enableMassActionMenuItems);
     $('#mass_edit_catalogs').on('click', openMassEditModal);
+    $('#makecatalogs').on('click', rebuildCatalogs());
     $(".chosen-select").chosen({width: "100%"});
 
     $(window).on('hashchange', function() {
