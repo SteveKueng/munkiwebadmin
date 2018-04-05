72d1d4157288bc39837e20e16c62c14093242040
Steve Küng <steve.kueng@srgssr.ch>
2018-04-05 11:00:48 +0200

    fix remove imagr

--- reports/admin.py
+++ reports/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from reports.models import Machine, MunkiReport, BusinessUnit, ImagrReport
+from reports.models import Machine, MunkiReport, BusinessUnit
 
 class BusinessUnitAdmin(admin.ModelAdmin):
     list_display = ('hash', 'name')
@@ -10,10 +10,6 @@ class MachineAdmin(admin.ModelAdmin):
 class MunkiReportAdmin(admin.ModelAdmin):
     list_display = ('hostname', 'timestamp', 'errors', 'warnings')
 
-class ImagrReportAdmin(admin.ModelAdmin):
-    list_display = ('machine', 'status', 'message', 'date_added')
-
 admin.site.register(BusinessUnit, BusinessUnitAdmin)
 admin.site.register(Machine, MachineAdmin)
 admin.site.register(MunkiReport, MunkiReportAdmin)
-admin.site.register(ImagrReport, ImagrReportAdmin)
