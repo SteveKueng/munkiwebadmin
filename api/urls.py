9fc459852d721e705563799bb2c068c72effabf2
Steve Küng <steve.kueng@srgssr.ch>
2019-05-27 15:24:44 +0200

    update urls.py

--- api/urls.py
+++ api/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     url(r'^(?P<kind>pkgs)/(?P<filepath>.*$)', api.views.file_api),
     url(r'^(?P<kind>vault)/(?P<subclass>[a-zA-Z]+$)', api.views.db_api),
     url(r'^(?P<kind>vault)/(?P<subclass>[a-zA-Z]+)/(?P<serial_number>[a-zA-Z0-9]+$)', api.views.db_api),
+    url(r'^(?P<kind>updates$)', api.views.updates_api),
     url(r'^(?P<kind>mdm)(?:/(?P<submission_type>.[a-zA-Z_]+))(?:/(?P<primary_id>[a-zA-Z0-9]+))(?:/(?P<action>[a-zA-Z_]+))(?:/(?P<secondary_id>[a-zA-Z0-9]+))?', api.views.mdm_api),
     url(r'^(?P<kind>mdm)(?:/(?P<submission_type>.[a-zA-Z_]+))(?:/(?P<primary_id>[a-zA-Z0-9]+))(?:/(?P<action>[a-zA-Z_]+))?', api.views.mdm_api),
     url(r'^(?P<kind>mdm)(?:/(?P<submission_type>.[a-zA-Z_]+))(?:/(?P<primary_id>[a-zA-Z0-9]+))?', api.views.mdm_api),
