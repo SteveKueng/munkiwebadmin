6ae037a0d5fcc36dab4b5b91100bdb4fdbac7cdb
SteveKueng <{ID}+{username}@users.noreply.github.com>
2023-10-06 21:42:19 +0200

    static files fix

--- app/munkiwebadmin/settings.py
+++ app/munkiwebadmin/settings.py
@@ -197,13 +197,6 @@ CACHES = {
 
 # needed by django-wsgiserver when using staticserve=collectstatic
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join(BASE_DIR, 'munkiwebadmin/static'),
-)
 
 # List of finder classes that know how to find static files in
 # various locations.
