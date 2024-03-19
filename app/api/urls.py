b3b6d0935a844f8287d4961575304aedce41a655
Steve Kueng <steve.kueng@umb.ch>
2024-03-19 22:10:52 +0100

    api url fix

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,19 +1,17 @@
 from django.urls import re_path as url
-import api.views
-
 from api.views import ReportsListAPIView, ReportsDetailAPIView, CatalogsListView, CatalogsDetailAPIView, ManifestsListView, ManifestsDetailAPIView, PkgsinfoListView, PkgsinfoDetailAPIView, PkgsListView, PkgsDetailAPIView, IconsListView, IconsDetailAPIView
 
 urlpatterns = [
-    url(r'^reports$', ReportsListAPIView.as_view(), name="report-list"),
+    url(r'^reports/?$', ReportsListAPIView.as_view(), name="report-list"),
     url(r'^reports/(?P<serial_number>[a-zA-Z0-9]+)$', ReportsDetailAPIView.as_view(), name="report-detail"),
-    url(r'^catalogs$', CatalogsListView.as_view(), name="catalog-list"),
+    url(r'^catalogs/?$', CatalogsListView.as_view(), name="catalog-list"),
     url(r'^catalogs/(?P<filepath>.*$)', CatalogsDetailAPIView.as_view(), name="catalog-detail"),
-    url(r'^manifests$', ManifestsListView.as_view(), name="manifest-list"),
+    url(r'^manifests/?$', ManifestsListView.as_view(), name="manifest-list"),
     url(r'^manifests/(?P<filepath>.*$)', ManifestsDetailAPIView.as_view()),
-    url(r'^pkgsinfo$', PkgsinfoListView.as_view(), name="pkgsinfo-list"),
+    url(r'^pkgsinfo/?$', PkgsinfoListView.as_view(), name="pkgsinfo-list"),
     url(r'^pkgsinfo/(?P<filepath>.*$)', PkgsinfoDetailAPIView.as_view(), name="pkgsinfo-detail"),
-    url(r'^pkgs$', PkgsListView.as_view(), name="pkgs-list"),
+    url(r'^pkgs/?$', PkgsListView.as_view(), name="pkgs-list"),
     url(r'^pkgs/(?P<filepath>.*$)', PkgsDetailAPIView.as_view(), name="pkgs-download"),
-    url(r'^icons$', IconsListView.as_view(), name="icon-list"),
+    url(r'^icons/?$', IconsListView.as_view(), name="icon-list"),
     url(r'^icons/(?P<filepath>.*$)', IconsDetailAPIView.as_view(), name="icon-download"),
 ]
