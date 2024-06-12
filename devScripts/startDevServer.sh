22fb12033e90bc5e34a52aab25260fcb1a5e9b11
Steve Küng <steve.kueng@gmail.com>
2024-06-12 21:37:15 +0200

    fix dev script

--- devScripts/startDevServer.sh
+++ devScripts/startDevServer.sh
@@ -5,19 +5,13 @@ export TIME_ZONE='Europe/Zurich'
 export LANGUAGE_CODE='en-us'
 export ALLOWED_HOSTS='localhost 127.0.0.1 [::1]'
 export DEFAULT_MANIFEST=''
+export PROXY_ADDRESS=''
 export DEBUG=1
-export MUNKI_REPO_DIR='https://munkiumb.blob.core.windows.net/munkirepo'
-export MUNKI_REPO_PLUGIN='AzureRepo'
-export MUNKITOOLS_DIR='/usr/local/munki'
+export MUNKI_REPO_DIR='/Users/Shared/munki_repo'
+export MAKECATALOGS_PATH='/usr/local/munki/makecatalogs'
+export FIELD_ENCRYPTION_KEY='VDKEyIzST-hbtX7rvA7LPue63E0XB0m3pZEFWKk0BKI='
 export REPO_MANAGEMENT_ONLY='False'
 
-
-# Create the munki repo directory
-if [ ! -d $MUNKI_REPO_DIR ]; then
-    mkdir -p $MUNKI_REPO_DIR
-fi
-
-cd app
 
 python manage.py makemigrations manifests pkgsinfo process reports inventory
 python manage.py migrate --noinput
