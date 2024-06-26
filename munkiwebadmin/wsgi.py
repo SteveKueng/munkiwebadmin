8938112d20c416d7d37e30875207b32096d4b3c5
Steve Küng <steve.kueng@gmail.com>
2024-06-26 15:00:10 +0200

    update wsgi

--- munkiwebadmin/wsgi.py
+++ munkiwebadmin/wsgi.py
@@ -1,20 +1,8 @@
-"""
-WSGI config for munkiwebadmin project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
-
-cert = os.path.join('/usr/local/share/ca-certificates','ca-certificates.crt')
-if os.path.exists(cert):
-    os.environ['REQUESTS_CA_BUNDLE'] = cert
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "munkiwebadmin.settings")
 
 application = get_wsgi_application()
+
+
