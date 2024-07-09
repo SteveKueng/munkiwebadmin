b46e10b6261ab060ad4645850cc892a36bfa8dcb
Steve Küng <steve.kueng@gmail.com>
2024-07-09 21:38:11 +0200

    fix migrations

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -24,7 +24,7 @@ if [ "$DATABASE" == "postgres" ]; then
 fi
 
 # migrate database
-python manage.py makemigrations catalogs, pkgsinfo, reports, manifests, inventory, icons, santa
+python manage.py makemigrations catalogs, process, pkgsinfo, reports, manifests, inventory, icons, santa
 python manage.py migrate
 
 echo "Starting $NAME as `whoami`"
