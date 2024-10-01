90dc660fdf044d12ce6d3b1fdcf4ee7187252b3d
Steve Kueng <steve.kueng@umb.ch>
2024-10-01 18:23:47 +0200

    fix azure startup script

--- entrypoint.azure.sh
+++ entrypoint.azure.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # migrate database
-python manage.py makemigrations catalogs, process, pkgsinfo, reports, manifests, inventory, icons, santa
+python manage.py makemigrations catalogs pkgsinfo reports manifests inventory icons santa process
 python manage.py migrate
 
 gunicorn --bind=0.0.0.0 --timeout 600 --workers=4 munkiwebadmin.wsgi
