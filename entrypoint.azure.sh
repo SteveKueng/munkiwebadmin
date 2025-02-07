876698d3b6b54f41812c0c283d232050e8b44f94
Steve Küng <steve.kueng@gmail.com>
2025-02-07 16:21:27 +0100

    update azure entrypoint

--- entrypoint.azure.sh
+++ entrypoint.azure.sh
@@ -4,4 +4,6 @@
 python manage.py makemigrations catalogs pkgsinfo reports manifests inventory icons santa process
 python manage.py migrate
 
+python manage.py collectstatic --noinput
+
 gunicorn --bind=0.0.0.0 --timeout 600 --workers=4 munkiwebadmin.wsgi
